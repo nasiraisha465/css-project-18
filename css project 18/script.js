f7625b44765eaf68ebdc0745365e4f4f428d1f39
nasiraisha465 <nasiraisha465@gmail.com>
2024-11-24 20:15:11 +0100

    Add files via upload

---
+++ css project 18/script.js
@@ -0,0 +1,5 @@
+var fruits =["strawberry","banana", "grapes", "raseberry", "kiwi"];
+document.getElementById("demo").innerHTML = fruits;
+fruits.pop()
+document.getElementById("demo1").innerHTML = fruits;
+fruits.pop()
